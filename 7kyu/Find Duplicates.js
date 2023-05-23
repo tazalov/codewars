d5908f6dbc14a4ea0a2dc6dbdbbb9061f3d71655
tazalov <tazalov9@gmail.com>
2023-05-23 08:23:04 +0300

    Add 7 kyu kata

---
+++ 7kyu/Find Duplicates.js
@@ -0,0 +1,21 @@
+/* 
+Given an array, find the duplicates in that array, and return a new array of those duplicates. The elements of the returned array should appear in the order when they first appeared as duplicates.
+
+Note: numbers and their corresponding string representations should not be treated as duplicates (i.e., "1" != 1).
+
+Examples
+[1, 2, 4, 4, 3, 3, 1, 5, 3, "5"]  ==>  [4, 3, 1]
+[0, 1, 2, 3, 4, 5]                ==>  []
+*/
+
+function duplicates(arr) {
+  const resultArray = [];
+  const badArray = [];
+  for (let i = 0; i < arr.length; i++) {
+    badArray.push(arr[i]);
+    if (badArray.includes(arr[i + 1]) && !resultArray.includes(arr[i + 1])) {
+      resultArray.push(arr[i + 1]);
+    }
+  }
+  return resultArray;
+}
