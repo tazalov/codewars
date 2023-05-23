2affb919c7ab7b33ec05c3a444eba8dde609f89b
tazalov <tazalov9@gmail.com>
2023-05-23 11:02:57 +0300

    Add 8 kyu kata (x2)

---
+++ 8kyu/Opposite number.js
@@ -0,0 +1,13 @@
+/* 
+Very simple, given an integer or a floating-point number, find its opposite.
+
+Examples:
+
+1: -1
+14: -14
+-34: 34
+*/
+
+function opposite(number) {
+  return number > 0 ? 0 - number : number * -1;
+}
