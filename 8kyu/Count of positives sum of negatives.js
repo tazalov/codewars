7c0edbba88b70984edf2be26f682fb8ad313680e
tazalov <tazalov9@gmail.com>
2023-05-25 08:28:25 +0300

    Add 8 kyu kata (x3)

---
+++ 8kyu/Count of positives sum of negatives.js
@@ -0,0 +1,32 @@
+/* 
+Given an array of integers.
+
+Return an array, where the first element is the count of positives numbers and the second element is sum of negative numbers. 0 is neither positive nor negative.
+
+If the input is an empty array or is null, return an empty array.
+
+Example
+For input [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15], you should return [10, -65].
+*/
+
+function countPositivesSumNegatives(input) {
+  if (input === null || !input.length) {
+    return [];
+  } else {
+    let resultArray = [];
+    let maxPositive = input.reduce((acc, el) => {
+      if (el > 0) {
+        acc++;
+      }
+      return acc;
+    }, 0);
+    let summNegative = input.reduce((acc, el) => {
+      if (el < 0) {
+        acc += el;
+      }
+      return acc;
+    }, 0);
+    resultArray.push(maxPositive, summNegative);
+    return resultArray;
+  }
+}
