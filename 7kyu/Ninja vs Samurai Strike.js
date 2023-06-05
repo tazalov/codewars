d708a0231c12ff8e2c432764905c639f9e4bc8a1
tazalov <tazalov9@gmail.com>
2023-06-05 08:37:31 +0300

    Add 7 kyu kata

---
+++ 7kyu/Ninja vs Samurai Strike.js
@@ -0,0 +1,18 @@
+/* 
+Something is wrong with our Warrior class. The strike method does not work correctly. The following shows an example of this code being used:
+
+var ninja = new Warrior('Ninja');
+var samurai = new Warrior('Samurai');
+
+samurai.strike(ninja, 3);
+ninja.health should == 70
+*/
+
+var Warrior = function (name) {
+  this.name = name;
+  this.health = 100;
+};
+
+Warrior.prototype.strike = function (enemy, swings) {
+  return (enemy.health = Math.max(0, enemy.health - swings * 10));
+};
