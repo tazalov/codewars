8213a63be8a31fa1c92d926aaf47449d171a8706
tazalov <tazalov9@gmail.com>
2023-06-06 08:02:49 +0300

    Add 6 kyu kata

---
+++ 6kyu/Coding Meetup #16.js
@@ -0,0 +1,33 @@
+/* 
+write a function that
+
+adds the question property to each object in the input array where the developer has not provided the relevant details (marked with a null value in JavaScript, with the default value in COBOL). The value of the question property should be the following string:
+Hi, could you please provide your <property name>.
+
+and returns only the developers with missing details:
+[
+  { firstName: null, lastName: 'I.', country: 'Argentina', continent: 'Americas', age: 35, language: 'Java', 
+  question: 'Hi, could you please provide your firstName.' },
+  { firstName: 'Lukas', lastName: 'X.', country: 'Croatia', continent: 'Europe', age: 35, language: null, 
+  question: 'Hi, could you please provide your language.' }
+]
+Notes:
+
+At most only one of the values will be null / empty.
+Preserve the order of the original list.
+Return an empty array [] if there is no developer with missing details.
+The input array will always be valid and formatted as in the example above.
+*/
+
+function askForMissingDetails(list) {
+  return list
+    .map((el) => {
+      for (const key in el) {
+        if (el[key] === null) {
+          el.question = `Hi, could you please provide your ${key}.`;
+          return el;
+        }
+      }
+    })
+    .filter((el) => el !== undefined);
+}
