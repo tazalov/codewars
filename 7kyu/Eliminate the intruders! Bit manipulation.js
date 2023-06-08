a161d3deaabb47fd5779dabc9ed282c52e034078
tazalov <tazalov9@gmail.com>
2023-06-08 08:49:29 +0300

    Add 7 kyu kata

---
+++ 7kyu/Eliminate the intruders! Bit manipulation.js
@@ -0,0 +1,18 @@
+/* 
+You are given a string representing a number in binary. Your task is to delete all the unset bits in this string and return the corresponding number (after keeping only the '1's).
+
+In practice, you should implement this function:
+
+function eliminateUnsetBits(number);
+Examples
+eliminateUnsetBits("11010101010101") ->  255 (= 11111111)
+eliminateUnsetBits("111") ->  7
+eliminateUnsetBits("1000000") -> 1
+eliminateUnsetBits("000") -> 0
+*/
+
+function eliminateUnsetBits(number) {
+  const newNumber = number.replace(/0/g, '');
+  if (!newNumber) return 0;
+  return parseInt(newNumber, 2);
+}
