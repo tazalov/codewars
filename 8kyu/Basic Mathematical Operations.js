e45e784194e89b623810ab72a403ecb121002bbe
tazalov <tazalov9@gmail.com>
2023-06-09 16:07:33 +0300

    Add 8 kyu kata

---
+++ 8kyu/Basic Mathematical Operations.js
@@ -0,0 +1,27 @@
+/* 
+Your task is to create a function that does four basic mathematical operations.
+
+The function should take three arguments - operation(string/char), value1(number), value2(number).
+The function should return result of numbers after applying the chosen operation.
+
+Examples(Operator, value1, value2) --> output
+('+', 4, 7) --> 11
+('-', 15, 18) --> -3
+('*', 5, 5) --> 25
+('/', 49, 7) --> 7
+*/
+
+function basicOp(operation, val1, val2) {
+  switch (operation) {
+    case '+':
+      return val1 + val2;
+    case '-':
+      return val1 - val2;
+    case '*':
+      return val1 * val2;
+    case '/':
+      return val1 / val2;
+    default:
+      return 0;
+  }
+}
