c4875e0a262fbdeb3ae0917f3cb3a5e5629cc5be
tazalov <tazalov9@gmail.com>
2023-06-09 07:51:38 +0300

    Add 8 kyu kata

---
+++ 8kyu/Total amount of points.js
@@ -0,0 +1,31 @@
+/* 
+Our football team has finished the championship.
+
+Our team's match results are recorded in a collection of strings. Each match is represented by a string in the format "x:y", where x is our team's score and y is our opponents score.
+
+For example: ["3:1", "2:2", "0:1", ...]
+
+Points are awarded for each match as follows:
+
+if x > y: 3 points (win)
+if x < y: 0 points (loss)
+if x = y: 1 point (tie)
+We need to write a function that takes this collection and returns the number of points our team (x) got in the championship by the rules given above.
+
+Notes:
+
+our team always plays 10 matches in the championship
+0 <= x <= 4
+0 <= y <= 4
+*/
+
+function points(games) {
+  const newGames = games.map((el) => el.split(':'));
+  let score = 0;
+  newGames.forEach((el) => {
+    if (el[0] > el[1]) score += 3;
+    if (el[0] === el[1]) score += 1;
+  });
+  return score;
+}
+//my map is fail((
