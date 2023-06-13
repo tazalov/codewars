5d9bf898505caf1db2d8b77b99e4bb08347263e4
tazalov <tazalov9@gmail.com>
2023-06-13 08:57:42 +0300

    Add 7 kyu kata

---
+++ 7kyu/Sort arrays - 1.js
@@ -0,0 +1,5 @@
+/* 
+Just a simple sorting usage. Create a function that returns the elements of the input-array / list sorted in lexicographical order.
+*/
+
+const sortme = (names) => names.sort();
