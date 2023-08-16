0aa40b7e69ed5944e1fa6559ebfc549c6fe7aec7
tazalov <tazalov9@gmail.com>
2023-08-16 17:35:58 +0300

    Add 7 kyu kata

---
+++ 7kyu/Two numbers are positive.js
@@ -0,0 +1,19 @@
+/* 
+Your job is to write a function, which takes three integers a, b, and c as arguments, and returns True if exactly two of of the three integers are positive numbers (greater than zero), and False - otherwise.
+
+Examples:
+twoArePositive(2, 4, -3) == true
+twoArePositive(-4, 6, 8) == true
+twoArePositive(4, -6, 9) == true
+twoArePositive(-4, 6, 0) == false
+twoArePositive(4, 6, 10) == false
+twoArePositive(-14, -3, -4) == false
+*/
+
+function twoArePositive(a, b, c) {
+    let count = 0
+    for (num of arguments) {
+        if (num > 0) count++
+    }
+    return count === 2
+}
