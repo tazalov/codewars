9b9a6b2fbdbd6c607c1a786b210f15a64ecc0bc4
tazalov <tazalov9@gmail.com>
2023-08-30 09:21:17 +0300

    Add 7 kyu kata

---
+++ 7kyu/Linked Lists - Push & BuildOneTwoThree.js
@@ -0,0 +1,41 @@
+/* 
+Linked Lists - Push & BuildOneTwoThree
+
+Write push() and buildOneTwoThree() functions to easily update and initialize linked lists. Try to use the push() function within your buildOneTwoThree() function.
+
+Here's an example of push() usage:
+
+var chained = null
+chained = push(chained, 3)
+chained = push(chained, 2)
+chained = push(chained, 1)
+push(chained, 8) === 8 -> 1 -> 2 -> 3 -> null
+The push function accepts head and data parameters, where head is either a node object or null/None/nil. Your push implementation should be able to create a new linked list/node when head is null/None/nil.
+
+The buildOneTwoThree function should create and return a linked list with three nodes: 1 -> 2 -> 3 -> null
+*/
+
+function Node(data, next = null) {
+  this.data = data;
+  this.next = next;
+}
+
+function push(head, data) {
+  return new Node(data, head);
+}
+
+function buildOneTwoThree() {
+  let data = 3;
+  let head = new Node(data);
+  while (data > 1) {
+    data--;
+    head = push(head, data);
+  }
+  return head;
+}
+
+
+//good job
+function buildOneTwoThree() {
+  return [3, 2, 1].reduce( (head, data) => push(head, data), null );
+}
