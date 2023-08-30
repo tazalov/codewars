1e7b713fc8a7078a7a5a992cc0270f56acd9cb32
tazalov <tazalov9@gmail.com>
2023-08-30 07:53:32 +0300

    Add 7 kyu kata

---
+++ 7kyu/Magic Index.js
@@ -0,0 +1,16 @@
+/* 
+A magic index in an array A[1...n-1] is defined to be an index such that A[i] = i. Given a sorted array of distinct integers, write a method to find a magic index, if one exists, in array A.
+
+findMagic([-20,-10,2,10,20]); // Returns 2
+*/
+
+function findMagic(arr) {
+  return arr.findIndex((el, i) => el === i);
+}
+
+//recursion
+function findMagic(arr, index = 0) {
+  if (!arr.length) return -1;
+  if (arr[0] === index) return index;
+  return findMagic(arr.slice(1), index + 1);
+}
